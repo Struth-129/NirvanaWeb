d012f9db612811283a92aa30389026edaa589846
Struth129 <sakets.ug18.ee@gmail.com>
2021-02-07 23:46:37 +0530

    Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import '../index.css';
+import '../sass/main.scss';
+import '../main';
+function Header () {
+	
+    return(
+        <>
+        <header id="header">
+			<h1><a href="index.html">Nirvana</a></h1>
+			<nav class="links">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Music</a></li>
+					<li><a href="#">Blogs</a></li>
+					<li><a href="#">About Us</a></li>
+				</ul>
+			</nav>
+			<nav class="main">
+					<ul>
+					<li class="search">
+						<a class="fa-search" href="#search">Search</a>
+						<form id="search" method="get" action="#">
+			    			<input type="text" name="query" placeholder="Search" />
+						</form>
+					</li>
+		    		<li class="menu">
+						<a class="fa-bars" href="#menu">Menu</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<section id="menu">
+							<section>
+								<form class="search" method="get" action="#" autoComplete="off">
+									<input type="text" name="query" placeholder="Search" autoComplete="off" />
+								</form>
+							</section>
+							<section>
+								<ul class="links">
+									<li>
+										<a href="#">
+											<h3>Nirvana Stress test</h3>
+											<p>Some content here</p>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Yoga/Medidation</h3>
+											<p>Some content here</p>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Take a tour</h3>
+											<p>Some content here</p>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<h3>Niri </h3>
+											<p>Some content here</p>
+										</a>
+									</li>
+								</ul>
+							</section>
+							<section>
+								<ul class="actions stacked">
+									<li><a href="#" class="button large fit">Log In</a></li>
+								</ul>
+								<ul class="actions stacked">
+									<li><a href="#" class="button large fit">Sign Up</a></li>
+								</ul>
+							</section>
+
+					</section>
+        </>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <Header/>
+  </>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// (function($) {
+
+// 	var	$window = $(window),
+// 		$body = $('body'),
+// 		$menu = $('#menu'),
+// 		$sidebar = $('#sidebar'),
+// 		$main = $('#main');
+
+// 	// Breakpoints.
+// 		breakpoints({
+// 			xlarge:   [ '1281px',  '1680px' ],
+// 			large:    [ '981px',   '1280px' ],
+// 			medium:   [ '737px',   '980px'  ],
+// 			small:    [ '481px',   '736px'  ],
+// 			xsmall:   [ null,      '480px'  ]
+// 		});
+
+// 	// Play initial animations on page load.
+// 		$window.on('load', function() {
+// 			window.setTimeout(function() {
+// 				$body.removeClass('is-preload');
+// 			}, 100);
+// 		});
+
+// 	// Menu.
+// 		$menu
+// 			.appendTo($body)
+// 			.panel({
+// 				delay: 500,
+// 				hideOnClick: true,
+// 				hideOnSwipe: true,
+// 				resetScroll: true,
+// 				resetForms: true,
+// 				side: 'right',
+// 				target: $body,
+// 				visibleClass: 'is-menu-visible'
+// 			});
+
+// 	// Search (header).
+// 		var $search = $('#search'),
+// 			$search_input = $search.find('input');
+
+// 		$body
+// 			.on('click', '[href="#search"]', function(event) {
+
+// 				event.preventDefault();
+
+// 				// Not visible?
+// 					if (!$search.hasClass('visible')) {
+
+// 						// Reset form.
+// 							$search[0].reset();
+
+// 						// Show.
+// 							$search.addClass('visible');
+
+// 						// Focus input.
+// 							$search_input.focus();
+
+// 					}
+
+// 			});
+
+// 		$search_input
+// 			.on('keydown', function(event) {
+
+// 				if (event.keyCode == 27)
+// 					$search_input.blur();
+
+// 			})
+// 			.on('blur', function() {
+// 				window.setTimeout(function() {
+// 					$search.removeClass('visible');
+// 				}, 100);
+// 			});
+
+// 	// Intro.
+// 		var $intro = $('#intro');
+
+// 		// Move to main on <=large, back to sidebar on >large.
+// 			breakpoints.on('<=large', function() {
+// 				$intro.prependTo($main);
+// 			});
+
+// 			breakpoints.on('>large', function() {
+// 				$intro.prependTo($sidebar);
+// 			});
+
+// })(jQuery);
